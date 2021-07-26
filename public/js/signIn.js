754dcc24350454c71c17d3bd356dd5d7e91425c4
Alyssa Umino <aumino.contact@gmail.com>
2021-07-26 17:43:01 +0000

    Redirect working

--- public/js/signIn.js
+++ public/js/signIn.js
@@ -1,4 +1,16 @@
 const signInButton = document.querySelector(".button");
 signInButton.addEventListener("click", () => {
-    console.log("clicked")
+    console.log("clicked");
+
+    //login authorization: create a popup that asks you to choose which account to sign in with
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(result => {
+        const user = result.user;
+
+        console.log("login success", user);
+        window.location = "writeNote.html";
+    })
+    .catch(error => {
+        console.log("login failed", error);
+    })
 })
