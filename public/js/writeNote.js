9fc4fe28c95ad00c3efed2306f3db954783912ec
Alyssa Umino <aumino.contact@gmail.com>
2021-07-26 18:48:55 +0000

    Personalized greeting message

--- public/js/writeNote.js
+++ public/js/writeNote.js
@@ -6,6 +6,7 @@ window.onload = () => {
             //this code runs if the user is logged in
             console.log("logged in as", user.displayName);
             googleUser = user;
+            document.querySelector("#greeting").innerHTML += ", " + googleUser.displayName + "?";
         }
         else {
             //this code runs if the user is not logged in
@@ -17,12 +18,14 @@ window.onload = () => {
     createNoteButton.addEventListener("click", () => {
         const noteTitle = document.querySelector("#noteTitle").value;
         const noteText = document.querySelector("#noteText").value;
-        console.log(noteTitle, noteText);
+        const timestamp = new Date();
+        console.log(noteTitle, noteText, timestamp);
 
         //write these values to the database
         firebase.database().ref(`/users/${googleUser.uid}`).push({
             title: noteTitle,
-            text: noteText
+            text: noteText,
+            created: timestamp
         }).then(() => {
             console.log("database write successful");
             document.querySelector("#noteTitle").value = "";
